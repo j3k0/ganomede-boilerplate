2930faf5828ac431c43428bae1ec303953f09685
Alexey Zabrodsky <elmigranto@gmail.com>
2017-03-27 17:30:55 +0500

    `debugInspect` test for circular objects.

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -3,6 +3,12 @@
 const utils = require('../src/utils');
 
 describe('utils', () => {
+  it('.debugInspect() works with circular objects with default options', () => {
+    const obj = {};
+    obj.self = obj;
+    expect(utils.debugInspect(obj)).to.contain('self: [Circular]');
+  });
+
   it('.hasOwnProperty()', () => {
     const obj = Object.create(null);
     obj.x = false;
